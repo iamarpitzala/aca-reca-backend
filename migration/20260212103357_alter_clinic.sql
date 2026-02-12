0fea1ff89b759e900721f55059b95a30a7297eef
mihirk-khode <mihirk@zenithive.com>
2026-02-12 16:19:30 +0530

    fix: Adjust SQL migration for clinic table
    - Refined the SQL migration script for the tbl_clinic table by separating the CHECK constraints for the method_type and is_active columns.
    - Removed the unnecessary 'AFTER description' clause for the is_active column to streamline the migration process.

--- migration/20260212103357_alter_clinic.sql
+++ migration/20260212103357_alter_clinic.sql
@@ -1,9 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE tbl_clinic
-    ADD COLUMN method_type VARCHAR(50) NOT NULL DEFAULT 'NET' CHECK (method_type IN ('NET', 'GROSS')),
-    ADD COLUMN is_active BOOLEAN NOT NULL DEFAULT TRUE CHECK (is_active IN (TRUE, FALSE)) AFTER description;
+    ADD COLUMN method_type VARCHAR(50) NOT NULL DEFAULT 'NET'
+        CHECK (method_type IN ('NET', 'GROSS')),
+    ADD COLUMN is_active BOOLEAN NOT NULL DEFAULT TRUE;
 -- +goose StatementEnd
+
 
 -- +goose Down
 -- +goose StatementBegin
